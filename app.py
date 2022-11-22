f671c9473698ad2b6e3c8c6e457ed9d27078d285
jacky_wang <user1@CM-2022080109>
2022-11-22 12:39:48 +0800

    update live stream

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ from init import app, db
 from models import User
 from forms import LoginForm, RegistrationForm
 from flask_socketio import SocketIO
+from flask import Flask, render_template, Response
+import cv2
 
 socketio = SocketIO(app)
+camera = cv2.VideoCapture(0)
+
 
 @app.route('/')
 def home():
@@ -45,11 +49,15 @@ def register():
         user = User(email=form.email.data,
         username=form.username.data, password=form.password.data)
         
-        # add to db table
-        db.session.add(user)
-        db.session.commit()
-        flash("感謝註冊本系統成為會員")
-        return redirect(url_for('login'))
+        try:
+            db.session.add(user)
+            db.session.commit()
+            flash("感謝註冊本系統成為會員")
+            return redirect(url_for('login'))
+
+        except Exception as e:
+            flash("您已註冊過本系統", "error")
+        
     return render_template('register.html',form=form)
 
 
@@ -65,6 +73,27 @@ def chat(message):
 @socketio.on('text')
 def test():
     socketio.send("test")
+
+@app.route('/live')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+def gen_frames():  
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
 
 
 if __name__ == '__main__':
